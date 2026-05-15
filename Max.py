d85c27c70039705585de088145a5e6c0fe85f80a
MasterMind20-08 <214620022+MasterMind20-08@users.noreply.github.com>
2026-05-15 15:51:22 +0300

    Creating window

---
+++ Max.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import tkinter as tk
+
+main_form = tk.Tk()
+
+main_form.title("Обработка миграционных данных")
+
+main_form.geometry("400x300")
+
+main_form.mainloop()
